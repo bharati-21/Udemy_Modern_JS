2ce88b1af9a02683298333b3f0d6e0e4250cb1c4
Bharati <bharatisharada@gmail.com>
2021-04-13 23:12:30 +0530

    Created folder for JS Fundamentals and completed logging to console

---
+++ 02_JS_Fundamentals/06_Using_Console.js
@@ -0,0 +1,22 @@
+// To log to the console:
+console.log('Hello World');
+
+console.log(123);
+console.log(true);
+var greeting = 'Hello World';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({a:1, b:2 });
+console.table({ a: 1, b: 2 });
+
+console.error('This is an error');
+console.warn('This is a warning');
+
+console.time('id');
+    console.log('Hello')
+    console.log('Hello')
+    console.log('Hello')
+    console.log('Hello')
+    console.log('Hello')
+    console.log('Hello')
+console.timeEnd('id');
