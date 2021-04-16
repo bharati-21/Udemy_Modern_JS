f37ba6ca6cbc1a3e6b85d47a743b6acf1fd60ede
Bharati <bharatisharada@gmail.com>
2021-04-16 13:17:52 +0530

    Completed 2.17 Switch

---
+++ 02_JS_Fundamentals/17_Switch/17_Switch.js
@@ -0,0 +1,39 @@
+const color = 'green';
+switch(color) {
+    case 'red':
+        console.log('Red Color');
+        break;
+    case 'blue':
+        console.log('Blue Color');
+        break;
+    case 'yellow':
+        console.log('Yellow Color');
+        break;
+    default:
+        console.log('Neither red, blue or yellow');
+        break;
+}
+
+switch ((new Date()).getDay()) {
+    case 0:
+        console.log('Sunday');
+        break;
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log('Tuesday');
+        break;
+    case 3:
+        console.log('Wednesday');
+        break;
+    case 4:
+        console.log('Thursday');
+        break;
+    case 5:
+        console.log('Friday');
+        break;
+    case 6:
+        console.log('Saturday');
+        break;
+}
