084cb09beb03931a5281c5ed4bacdb6745ec1a7b
Bharati <bharatisharada@gmail.com>
2021-04-22 22:02:43 +0530

    Completed 7.66 ES6 Promises

--- 07_AsyncJS_Ajax_FetchAPI/62_Callback_Functions/62_Callback_Functions.js
+++ 07_AsyncJS_Ajax_FetchAPI/62_Callback_Functions/62_Callback_Functions.js
@@ -23,11 +23,42 @@ const posts = [
 // createPost({ title: 'Post Four', body: 'This is Post Four' });
 // getPost();
 
-function createPost(post, callback) {
-    setTimeout(function () {
-        posts.push(post);
-        callback();
-    }, 2000);
+// function createPost(post, callback) {
+//     setTimeout(function () {
+//         posts.push(post);
+//         callback();
+//     }, 2000);
+// }
+
+// function getPost() {
+//     setTimeout(function () {
+//         let output = '';
+//         posts.forEach(function (post) {
+//             output += `<li>${post.title}</li>`
+//         });
+//         document.body.innerHTML = output;
+//     }, 1000);
+// }
+
+// createPost({ title: 'Post Four', body: 'This is Post Four' }, getPost);
+
+
+
+
+function createPost(post) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            posts.push(post);
+
+            let error = false;
+            if (!error) {
+                resolve();
+            }
+            else {
+                reject('Something went wrong');
+            }            
+        }, 2000);
+    });
 }
 
 function getPost() {
@@ -40,4 +71,6 @@ function getPost() {
     }, 1000);
 }
 
-createPost({ title: 'Post Four', body: 'This is Post Four' }, getPost);
+createPost({ title: 'Post Four', body: 'This is Post Four' }).then(getPost).catch(function (error) {
+    console.log(error);
+});
