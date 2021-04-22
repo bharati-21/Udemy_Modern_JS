e6e04875c31021b21585bd7a59560d6555620232
Bharati <bharatisharada@gmail.com>
2021-04-22 21:48:24 +0530

    Completed 7.64 and 7.65 Custom HTTP Library

---
+++ 07_AsyncJS_Ajax_FetchAPI/63_Custom_Library/app.js
@@ -0,0 +1,58 @@
+const http = new easyHTTP();
+
+// Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function (error, posts) {
+//     if (error) {
+//         console.log(error);
+//     }
+//     else {
+//         console.log(posts);
+//     }
+// });
+
+
+// Get Single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (error, post) {
+//     if (error) {
+//         console.log(error);
+//     }
+//     else {
+//         console.log(post);
+//     }
+// });
+
+// Post Request
+const data = {
+    title: 'Custom Post',
+    body: 'This is a Custom Post'
+};
+
+// Creat Post Request
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (error, post) {
+//     if (error) {
+//         console.log(error);
+//     }
+//     else {
+//         console.log(post);
+//     }
+// });
+
+// Creat put method
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (error, post) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(post);
+    }
+});
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (error, response) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(response);
+    }
+});
