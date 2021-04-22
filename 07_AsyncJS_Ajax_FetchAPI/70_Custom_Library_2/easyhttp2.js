499b45722b5c8515c89aec210654ee556f8af54c
Bharati <bharatisharada@gmail.com>
2021-04-22 23:23:19 +0530

    Completed 7.70 EasyHTTP Version 2

---
+++ 07_AsyncJS_Ajax_FetchAPI/70_Custom_Library_2/easyhttp2.js
@@ -0,0 +1,69 @@
+/**
+    * EasyHTTP Library
+    * Library for making HTTP requests
+    *
+    * @version 2.0.
+    * @author Bharati Subramanian
+    * @license MIT
+    *
+**/
+
+class EasyHTTP {
+    // Make an HTTP GET Request
+    get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(data));
+        });   
+    }
+
+    // Make HTTP POST Request
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(data));
+        });   
+    }
+
+    // Make a HTTP PUT Request
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(data));
+        });   
+    }
+
+    // Make a HTTP Delete Request
+    delete(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(() => resolve('Resource Deleted'))
+            .catch(err => reject(data));
+        });     
+    }
+}
