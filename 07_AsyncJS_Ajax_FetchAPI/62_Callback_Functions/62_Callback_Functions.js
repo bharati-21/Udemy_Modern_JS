6a3c7b02b8b9bb14fec182fc67983faedbfa4f05
Bharati <bharatisharada@gmail.com>
2021-04-22 18:54:48 +0530

    Completed 7.62 Callback Functions

---
+++ 07_AsyncJS_Ajax_FetchAPI/62_Callback_Functions/62_Callback_Functions.js
@@ -0,0 +1,43 @@
+const posts = [
+    {title: 'Post One', body: 'This is Post One'},
+    {title: 'Post Two', body: 'This is Post Two'},
+    {title: 'Post Three', body: 'This is Post Three'},
+]
+
+// function createPost(post) {
+//     setTimeout(function () {
+//         posts.push(post);
+//     }, 2000);
+// }
+
+// function getPost() {
+//     setTimeout(function () {
+//         let output = '';
+//         posts.forEach(function (post) {
+//             output += `<li>${post.title}</li>`
+//         });
+//         document.body.innerHTML = output;
+//     }, 1000);
+// }
+
+// createPost({ title: 'Post Four', body: 'This is Post Four' });
+// getPost();
+
+function createPost(post, callback) {
+    setTimeout(function () {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+function getPost() {
+    setTimeout(function () {
+        let output = '';
+        posts.forEach(function (post) {
+            output += `<li>${post.title}</li>`
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+createPost({ title: 'Post Four', body: 'This is Post Four' }, getPost);
