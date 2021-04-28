22316ae607d1925b1b8923010aac7b6b35d9323c
Bharati <bharatisharada@gmail.com>
2021-04-28 23:30:59 +0530

    Started app.js for Loan Calculator Project

---
+++ 04_DOM_Projects/35_Loan_Calculator/app.js
@@ -0,0 +1,15 @@
+// Listen for button submit
+document.querySelector('#loan-form').addEventListener('submit', calculateLoan);
+
+// Calculate Loan
+function calculateLoan(e) {
+    e.preventDefault();
+
+    // UI variables
+
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    
+}
+
