f3423c7acf531543858b4eb2129a358046875dda
Bharati <bharatisharada@gmail.com>
2021-05-02 23:17:49 +0530

    Async Await

--- 12_Async_JS/promises.js
+++ 12_Async_JS/promises.js
@@ -44,14 +44,34 @@ function createPost(post) {
 // createPost({ title: 'Post 4', body: 'This is Post 4' })
 //     .then(getPosts, err => document.body.innerHTML = err);
 
-// Prmoise.all
+/* Async / Await */
 
-const promise1 = Promise.resolve('Hello World');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 2000, 'Goodbye');
-});
+async function init() {
+    await createPost({ title: 'Post 4', body: 'This is Post 4' });
+    getPosts();
+}
 
-const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+init();
 
-Promise.all([promise1, promise2, promise3, promise4]).then((values) => console.log(values));
+/* Promise.all */
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 2000, 'Goodbye');
+// });
+
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+// Promise.all([promise1, promise2, promise3, promise4]).then((values) => console.log(values));
+
+// Async Await Fetch
+async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    const data = await res.json();
+
+    console.log(data);
+}
+
+fetchUsers();
+
