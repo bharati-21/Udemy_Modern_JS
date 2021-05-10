3df4f2b3c80ce06076a9b0dda07ef387907fe8ea
Bharati <bharatisharada@gmail.com>
2021-05-10 20:26:42 +0530

    Added local storage and completed WeatherJS

--- 08_API_Projects/78_Weather_API/app.js
+++ 08_API_Projects/78_Weather_API/app.js
@@ -1,9 +1,26 @@
-const weather = new Weather('London');
+const storage = new Storage();
+const weatherLoc = storage.getLocationData();
+
+const weather = new Weather(weatherLoc);
+
 const ui = new UI();
+
 
 // weather.changeLocation('Varanasi');
 document.addEventListener('DOMContentLoaded',getWeather )
 
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    weather.changeLocation(city);
+
+    storage.setLocationData(city);
+
+    getWeather();
+
+    $('#locModal').modal('hide');
+    document.getElementById('city').value = '';
+});
 
 function getWeather() {
     weather.getWeather(weather)
--- 08_API_Projects/78_Weather_API/ui.js
+++ 08_API_Projects/78_Weather_API/ui.js
@@ -16,7 +16,7 @@ class UI {
         this.desc.textContent = weather.current.condition.text;
         this.string.textContent = `${weather.current.temp_c}°C / ${weather.current.temp_f}°F`;
         this.icon.setAttribute('src', weather.current.condition.icon);
-        this.humidity.textContent = 'Humidity: ' + weather.current.humidity;
+        this.humidity.textContent = 'Humidity: ' + weather.current.humidity +'%';
         this.feelsLike.textContent = `Feels Like: ${weather.current.feelslike_c}°C / ${weather.current.feelslike_f}°F`;
         this.wind.textContent = `Wind: From ${weather.current.wind_dir} at ${weather.current.wind_mph} MPH`;
     }
